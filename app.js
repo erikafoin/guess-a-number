3fdfc56bdd8a9b014d2a34833dcd237666a78f0a
Ben Beekman <bbeekman@gmail.com>
2019-10-02 12:21:40 -0700

    small fixes

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 import { compareNumbers } from './compare-numbers.js';
 
-
 const guessButton = document.getElementById('guess-button');
 let guessInput = document.getElementById('guess-input');
 let guessesLeft = document.getElementById('guesses-left');
 let result = document.getElementById('result');
+const instructions = document.getElementById('instructions');
 
 
 // Set initial state
-guessInput.value = 0;
+guessInput.value = 10;
 let guesses = 4;
 const randomNumber = Math.floor((Math.random() * 20) + 1);
-console.log(randomNumber);
 
 // Set Event handlers
 guessButton.addEventListener('click', () => {
+    instructions.textContent = '';
     if (guesses > 0) {
         guesses--;
         guessesLeft.textContent = guesses;
-        let guess = guessInput.value;
+        let guess = Number(guessInput.value);
 
         if ((compareNumbers(guess, randomNumber) === 1)) {
             result.textContent = 'Too high!';
-            if (guesses < 1) {
-                result.textContent = 'No more guesses. Game over!';
-            }
+
         }
         else if (compareNumbers(guess, randomNumber) === -1) {
             result.textContent = 'Too low!';
-            if (guesses < 1) {
-                result.textContent = 'No more guesses. Game over!';
-            }
         }
-        else {
+        else if (compareNumbers(guess, randomNumber) === 0) {
             result.textContent = 'Congratulations! You win!';
+            result.style.color = 'green';
+            guessButton.disabled = true;
+        }
+
+        else {
+            result.textContent = 'Unexpected output.';
         }
     }
-    else return;
+    gameOverCheck();
 });
+
+function gameOverCheck() {
+    if (guesses < 1) {
+        result.textContent = 'No more guesses. Game over!';
+        result.style.color = 'red';
+        guessButton.disabled = true;
+    }
+}
