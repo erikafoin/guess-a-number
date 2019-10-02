80d01bd7fae7dbf6f958b245fb96696f7c2e68f7
Ben Beekman <bbeekman@gmail.com>
2019-10-01 17:00:31 -0700

    working tests

---
+++ compare-numbers.js
@@ -0,0 +1,6 @@
+const compareNumbers = (guess, correctNumbers) => {
+	if (guess === correctNumbers) return 0;
+	else if (guess > correctNumbers) return 1;
+	else if (guess < correctNumbers) return -1;
+};
+export default compareNumbers;
